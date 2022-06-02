caa3f75a7f1f385da79e60c0faed4f17accbd107
Yang Chen <zhiyang.chen@rubylife.com>
2022-06-02 17:33:43 -0400

    commit 5

--- vars/pipelineVars1.groovy
+++ vars/pipelineVars1.groovy
@@ -1,3 +1,13 @@
 def call(message) {
-    echo "Hello World! ${message}"
+    echo "${message}"
+    setup()
+    def p = null
+    p.add(string(name: 'ENV', defaultValue: message, description: 'target environment'))
+    properties([
+        parameters(p)
+    ])
+}
+
+def setup() {
+    echo params.ENV
 }
